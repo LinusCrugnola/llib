f889b854978b9d78ee09633170970f5e45b13441
Linus Crugnola <linus.crugnola@epfl.ch>
2022-10-01 19:03:10 +0200

    Revert "added some basic notation"
    This reverts commit 3968ab6156054b1efc6cfbde2654f23c3a484c24.

--- include/baseexpr.h
+++
@@ -1,3 +0,0 @@
-#define _CONSTEXPR constexpr
-#define _NODISCARD [[nodiscard]]
-#define _LLIB_TPL template
--- include/vec2d.h
+++ include/vec2d.h
@@ -10,7 +10,6 @@
  
 #pragma once
 
-#include "baseexpr.h"
 #include <iostream>
 #include <math.h>
 
@@ -22,7 +21,7 @@ namespace llib {
      * @param dx change of x direction
      * @param dy change of y direciton
      */
-    _LLIB_TPL<class T>
+    template<class T>
     struct vec2d final {
         T _x;
         T _y;
@@ -35,36 +34,36 @@ namespace llib {
         // disable the copy constructor
         //vec2d(const vec2d&) = delete;
 
-        _NODISCARD vec2d<T> operator+(const vec2d<T>& v) {
+        [[nodiscard]] vec2d<T> operator+(const vec2d<T>& v) {
             return vec2d<T>(_x + v._x, _y + v._y);
         }
-        _NODISCARD vec2d<T> operator-(const vec2d<T>& v){
+        [[nodiscard]] vec2d<T> operator-(const vec2d<T>& v){
             return vec2d<T>(_x - v._x, _y - v._y);
         }
-        _NODISCARD bool operator==(const vec2d<T>& v) const {
+        [[nodiscard]] bool operator==(const vec2d<T>& v) const {
             return (_x == v._x) and (_y == v._y);
         }
-        _NODISCARD bool operator!=(const vec2d<T>& v) const{
+        [[nodiscard]] bool operator!=(const vec2d<T>& v) const{
             return (_x != v._x) or (_y != v._y);
         }
-        _NODISCARD float len() const {
+        [[nodiscard]] float len() const {
             return sqrt(_x * _x + _y * _y);
         }
-        _NODISCARD float ang() const {
+        [[nodiscard]] float ang() const {
             return atanf(_y / _x);
         }
         //TODO: weird behaviour sometimes (nan)
-        _CONSTEXPR void setLen(const float& length){
+        constexpr void setLen(const float& length){
             float _fac = (float)_y / _x;
             _x = length * 1 / sqrtf32(1 + _fac);
             _y = length * _fac / sqrtf32(1 + _fac);
         }
-        _CONSTEXPR void setAng(const float& angle){
+        constexpr void setAng(const float& angle){
             float _fac = this->len();
             _x = _fac * cosf32(angle);
             _y = _fac * sinf32(angle);
         }
-        _CONSTEXPR void rotate(const float& angle){
+        constexpr void rotate(const float& angle){
             _x = cosf32(angle) * _x - sinf32(angle) * _y;
             _y = sinf32(angle) * _x + cosf32(angle) * _y;
         }
@@ -73,22 +72,22 @@ namespace llib {
 
     };
 
-    _LLIB_TPL<class T>
+    template<class T>
     vec2d<T> operator*(const float& n, const vec2d<T>& vr){
         return vec2d<T>(vr._x * n, vr._y * n);
     }
 
-    _LLIB_TPL<class T>
+    template<class T>
     vec2d<T> operator*(const vec2d<T>& vl, const float& n){
         return vec2d<T>(vl._x * n, vl._y * n);
     }
 
-    _LLIB_TPL<class T>
+    template<class T>
     float scalProd(const vec2d<T>& v1, const vec2d<T>& v2){
         return v1._x * v2._x + v1._y * v2._y;
     }
 
-    _LLIB_TPL<class T>
+    template<class T>
     std::ostream& operator<<(std::ostream& os, const vec2d<T>& v){
         os << "( " << v._x << ", " << v._y << " )";
         return os;
